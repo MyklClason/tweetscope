dd00a56bce6845be97d7061603698e7d344f38e6
Mykl Clason <myklclason@gmail.com>
2016-08-01 06:57:17 +0000

    Add latest tweets

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,18 @@
 class Keyword < ActiveRecord::Base
+  def grab_tweets
+    count = 5
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
+    
+    client
+    .search(self.word, count: count, result_type: "recent")
+    .take(count)
+    .collect do |tweet|
+      "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+  end
 end
